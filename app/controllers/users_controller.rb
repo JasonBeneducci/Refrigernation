c6db979f870081637eb9dc31bf3e99f137bd8de9
Andy Choi <wooseokchoi96@gmail.com>
2019-09-10 10:15:20 -0400

    created session when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else 
       flash.now[:messages] = @user.errors.full_messages
